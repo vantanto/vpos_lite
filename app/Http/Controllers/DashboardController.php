05e8d4b379d234c0691aab65084f3d64dc198ed0
vantanto <56611021+vantanto@users.noreply.github.com>
2022-12-22 10:38:23 +0700

    add: dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,12 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Customer;
+use App\Models\Order;
+use App\Models\Purchase;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     public function index(Request $request)
     {
-        return view('dashboard');
+        $dateStart = Carbon::now()->startOfDay();
+        $dateEnd = Carbon::now()->endOfDay();
+        $dateStartMonth = Carbon::now()->startOfMonth();
+
+        $newOrder = Order::whereBetween('date', [$dateStart, $dateEnd])
+            ->count();
+        $totalOrder = Order::whereBetween('date', [$dateStart, $dateEnd])
+            ->sum('total');
+        $newPurchase = Purchase::whereBetween('date', [$dateStart, $dateEnd])
+            ->count();
+        $newCustomer = Customer::whereBetween('created_at', [$dateStartMonth, $dateEnd])
+            ->count();
+        
+        return view('dashboard', compact('newOrder', 'totalOrder', 'newPurchase', 'newCustomer'));
     }
 }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,25 +1,57 @@
 @extends('layouts.app')
+@section('header')
+<div class="col-sm-6">
+    <h1>Dashboard</h1>
+</div>
+<div class="col-sm-6">
+    <ol class="breadcrumb float-sm-right">
+        <li class="breadcrumb-item active">Dashboard</li>
+    </ol>
+</div>
+@endsection
 @section('content')
 <section class="content">
-    <div class="card">
-        <div class="card-header">
-            <h3 class="card-title">Title</h3>
-
-            <div class="card-tools">
-                <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
-                    <i class="fas fa-minus"></i>
-                </button>
-                <button type="button" class="btn btn-tool" data-card-widget="remove" title="Remove">
-                    <i class="fas fa-times"></i>
-                </button>
+    <div class="container-fluid">
+        @can('admin')
+        <div class="row">
+            <div class="col-lg-3 col-6">
+                <div class="small-box bg-success">
+                    <div class="inner">
+                        <h3>{{ Helper::numberFormatNoZeroes($newOrder) }}</h3>
+                        <p>New Order</p>
+                    </div>
+                    <div class="icon"><i class="fas fa-shopping-cart"></i></div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-6">
+                <div class="small-box bg-info">
+                    <div class="inner">
+                        <h3>{{ Helper::numberFormatNoZeroes($totalOrder) }}</h3>
+                        <p>Total Order</p>
+                    </div>
+                    <div class="icon"><i class="fas fa-dollar-sign"></i></div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-6">
+                <div class="small-box bg-danger">
+                    <div class="inner">
+                        <h3>{{ Helper::numberFormatNoZeroes($newPurchase) }}</h3>
+                        <p>New Purchase</p>
+                    </div>
+                    <div class="icon"><i class="fas fa-cart-plus"></i></div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-6">
+                <div class="small-box bg-warning">
+                    <div class="inner">
+                        <h3>{{ Helper::numberFormatNoZeroes($newCustomer) }}</h3>
+                        <p>New Customer</p>
+                    </div>
+                    <div class="icon"><i class="fas fa-user"></i></div>
+                </div>
             </div>
         </div>
-        <div class="card-body">
-            Start creating your amazing application!
-        </div>
-        <div class="card-footer">
-            Footer
-        </div>
+        @endcan
     </div>
 </section>
 @endsection
